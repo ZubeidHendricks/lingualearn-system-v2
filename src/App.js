59b563b51b5c5de36c62867502c15c8448c2ee0f
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-17 06:13:00 +0200

    Add basic React app structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LinguaLearnApp } from './lingualearn/ui/LinguaLearnApp';
+
+function App() {
+  return (
+    <div className="h-screen">
+      <LinguaLearnApp />
+    </div>
+  );
+}
+
+export default App;
