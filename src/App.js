dd90f0650f87c99b718068d8837eec3c5560f183
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-17 06:22:30 +0200

    Add React app entry point files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { LinguaLearnApp } from './lingualearn/ui/LinguaLearnApp';
 
 function App() {
   return (
-    <div className="h-screen">
+    <div className="h-screen bg-gray-100">
       <LinguaLearnApp />
     </div>
   );
