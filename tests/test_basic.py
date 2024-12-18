5e9a11b9c9e2785d9762a212b944b7935f82780b
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-18 09:34:42 +0200

    Simplify to basic test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,2 +1,9 @@
-def test_simple():
-    assert True
+from fastapi.testclient import TestClient
+from src.backend.main import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
